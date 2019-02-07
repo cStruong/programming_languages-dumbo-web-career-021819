f1433fedae04ad216e6f10e7ebe07808fefc74ff
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-07 18:08:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    
+  #[oo/functional], [ruby/javas/pyt/java/clos/er/sca/javas]
+  languages.each { |firstKey, firstValue|
+    #[ruby/javas/pyt/java/clos/er/sca/javas], [type]
+    firstValue.each { |secondKey, secondValue|
+      new_hash[secondKey] = secondValue
+      #[type][interpreted/compiled]
+      #secondValue.each { |thirdKey, thirdValue|}
+      #new_hash[secondKey]
+    }
+  }
+    
+  binding.pry
+  return new_hash
 end
